9f314b77784254d2cf0d41b25e70d06592d2a494
AV-63-dev <amanin63@mail.ru>
2023-01-16 18:57:57 +0400

    done task

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,24 +1,155 @@
 import request from "supertest";
 import app from './app';
+import { IUser } from './interface';
+import { message } from './messages';
+import { validate, v4 } from 'uuid';
 
 const server = request(app);
 
-describe('Scenario', () => {
+const url = '/api/users';
+const testUser:IUser = {
+  username: 'Test',
+	age: 100,
+	hobbies: []
+};
+let uuid = '';
 
-  it('get', async () => {
-    const response = await server.get('/api/users');
-    expect(response.statusCode).toBe(200)
+describe('Scenario 1: from task', () => {
+  it('Get all users', async () => {
+    const response = await server.get(url);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
   });
 
-  it('get', async () => {
-    const response = await server.get('/api/users');
-    expect(response.statusCode).toBe(200)
+  it('Сreate user', async () => {
+    const response = await server.post(url).send(JSON.stringify(testUser));
+    expect(response.statusCode).toBe(201);
+    uuid = response.body.id;
+    expect(validate(uuid)).toBe(true);
+    testUser.id = uuid;
+    expect(response.body).toEqual(testUser);
   });
 
-  it('get', async () => {
-    const response = await server.get('/api/users');
-    expect(response.statusCode).toBe(200)
+  it('Get user', async () => {
+    const response = await server.get(`${url}/${uuid}`);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(testUser);
   });
 
-  app.close();
+  it('Update user', async () => {
+    testUser.hobbies = ['I love women'];
+    const response = await server.put(`${url}/${uuid}`).send(JSON.stringify(testUser));
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(testUser);
+  });
+
+  it('Delete user', async () => {
+    const response = await server.delete(`${url}/${uuid}`);
+    expect(response.statusCode).toBe(204);
+    expect(response.body).toEqual({});
+  });
+
+  it('Get no such user', async () => {
+    const response = await server.get(`${url}/${uuid}`);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.noData);
+  });
 });
+
+describe('Scenario 2: Errors request to GET, POST and ERROR=500', () => {
+  it('GET "/"', async () => {
+    const response = await server.get('/');
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.nonExistEndpoint);
+  });
+  
+  it('Get no such user, "id === uuid"', async () => {
+    const response = await server.get(`${url}/${v4()}`);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.noData);
+  });
+
+  it('Get user, "id !== uuid"', async () => {
+    const response = await server.get(`${url}/123`);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe(message.notUUID);
+  });
+
+  it('POST "/"', async () => {
+    const response = await server.post('/').send(JSON.stringify(testUser));
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.nonExistEndpoint);
+  });
+
+  it('POST "/api/users/{uuid}"', async () => {
+    const response = await server.post(`${url}/${v4()}`).send(JSON.stringify(testUser));
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.nonExistEndpoint);
+  });
+
+  it('Сreate user without required fields', async () => {
+    const response = await server.post(url).send(JSON.stringify({age: 1}));
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe(message.noValidBody);
+  });
+
+  it('POST "ERROR=500"', async () => {
+    const response = await server.post(url).send('{"username:"Test","age":100,"hobbies":[}');
+    expect(response.statusCode).toBe(500);
+    expect(response.body.message).toBe(message.serverError);
+  });
+
+  it('Server is working', async () => {
+    const response = await server.get(url);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
+  });
+});
+
+describe('Scenario 3: Errors request to PUT and DELETE', () => {
+  it('PUT "/api/users"', async () => {
+    const response = await server.put(url).send(JSON.stringify(testUser));
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.nonExistEndpoint);
+  });
+
+  it('Update no such user, "id === uuid"', async () => {
+    const response = await server.put(`${url}/${v4()}`).send(JSON.stringify(testUser));
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.noData);
+  });
+
+  it('Update user, "id !== uuid"', async () => {
+    const response = await server.put(`${url}/123}`).send(JSON.stringify(testUser));
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe(message.notUUID);
+  });
+
+  it('Update user, without required fields', async () => {
+    const newUser = await server.post(url).send(JSON.stringify(testUser));
+    uuid = newUser.body.id;
+    const response = await server.put(`${url}/${uuid}`).send(JSON.stringify({age: 18}));
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe(message.noValidBody);
+  });
+
+  it('DELETE "/api/users"', async () => {
+    const response = await server.delete(url);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.nonExistEndpoint);
+  });
+
+  it('Delete no such user, "id === uuid"', async () => {
+    const response = await server.delete(`${url}/${v4()}`);
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe(message.noData);
+  });
+
+  it('Delete user, "id !== uuid"', async () => {
+    const response = await server.delete(`${url}/123`);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe(message.notUUID);
+  });
+});
+
+app.close();
--- src/messages.ts
+++ src/messages.ts
@@ -4,15 +4,23 @@ export const messageBalancerStart = () => console.log(`Balancer is running on po
 
 export const messagePortProcessingServer = () => console.log(`Processing Server on port ${process.env.PORT}`);
 
-export const messageServerError = () => {
-    console.log(`Server error (code 500)`);
-    return {code: 500, body: 'Server error'};
+export const message = {
+    serverError: 'Server error',
+    nonExistEndpoint: 'Non exist endpoint',
+    notUUID: 'Requested ID is not UUID',
+    noData: 'Is no such data',
+    noValidBody: 'No required fields in request body',
 };
 
-export const messageNonExistEndpoint = () => { return {code: 404, body: 'Non exist endpoint'} };
+export const messageServerError = () => {
+    console.log(`Server error (code 500)`);
+    return {code: 500, body: message.serverError};
+};
 
-export const messageNotUUID = () => { return {code: 400, body: 'Requested ID is not UUID'} };
+export const messageNonExistEndpoint = () => { return {code: 404, body: message.nonExistEndpoint} };
 
-export const messageNoData = () => { return {code: 404, body: 'Is no such data'} };
+export const messageNotUUID = () => { return {code: 400, body: message.notUUID} };
 
-export const messageNoValidBody = () => { return {code: 400, body: 'No required fields in request body'} };
+export const messageNoData = () => { return {code: 404, body: message.noData} };
+
+export const messageNoValidBody = () => { return {code: 400, body: message.noValidBody} };
